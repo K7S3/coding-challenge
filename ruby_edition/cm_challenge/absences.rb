9406b21737df6f607905a4f0c78718ed72d562ff
K7S3 <keshavan.seshadri@research.iiit.ac.in>
2020-10-16 16:52:12 +0530

    Wrote code for getting ical files and summary

--- ruby_edition/cm_challenge/absences.rb
+++ ruby_edition/cm_challenge/absences.rb
@@ -1,9 +1,39 @@
 require_relative './api'
 
+require 'icalendar'
+
 module CmChallenge
   class Absences
-    def to_ical
-      # Your implementation here
+    
+    def initialize
+      @members  = CmChallenge::Api.members
+      @absences = CmChallenge::Api.absences
+      @absent_list = CmChallenge::Api.get_absence_list # Contains The list of absences along with names.
     end
+
+    def to_ical     
+      cal = Icalendar::Calendar.new
+      @absent_list.each do |absence|
+        cal.event do |e|
+          #Remove '-' and replace with ''
+          e.dtstart     = Icalendar::Values::Date.new(absence[:start_date].gsub('-', ''))
+          e.dtend       = Icalendar::Values::Date.new(absence[:end_date].gsub('-', ''))
+          e.summary     = get_summary(absence[:name], absence[:type])        
+        end
+        cal.publish
+      end
+      return cal
+    end
+
+    def get_summary name, type
+      if type.eql?("vacation")
+        return "#{name} is on vacation."
+      elsif type.eql?("sickness")
+        return "#{name} is sick."
+      else
+        return "#{name} is absent due to unknown reason."
+      end
+    end
+
   end
 end
